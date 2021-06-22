a4d9cca4537f9aa264c54b512591fb7784044758
Roydano <roy.dano90@gmail.com>
2021-06-22 16:12:19 +0200

    add immagini + struttura base css e html

--- js/script.js
+++ js/script.js
@@ -2,7 +2,15 @@ const app = new vue(
     {
         el: '#app',
         data:{
-            
+            img: [
+                'img/dbz.jpg',
+                'img/digimon.jpg',
+                'img/holly-e-benji.webp',
+                'img/naruto.jpg',
+                'img/one-piece.jpg',
+                'img/pokemon.jpg',
+                'img/saint-seya.jpg'
+            ]
         }
     }
 );
