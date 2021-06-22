cb2e0799ec7899829ff05fccadf2bfc33e58b376
Roydano <roy.dano90@gmail.com>
2021-06-22 16:55:55 +0200

    add funzione click verso destra +

--- js/script.js
+++ js/script.js
@@ -1,16 +1,37 @@
-const app = new vue(
+const app = new Vue(
     {
         el: '#app',
         data:{
-            img: [
+
+            counter: 0,
+            images: [
                 'img/dbz.jpg',
                 'img/digimon.jpg',
                 'img/holly-e-benji.webp',
                 'img/naruto.jpg',
                 'img/one-piece.jpg',
                 'img/pokemon.jpg',
-                'img/saint-seya.jpg'
-            ]
+                'img/saint-seiya.jpg'
+            ]            
+        },
+
+        methods:{
+
+            nextPhoto(){
+                //! CON TERNARIA
+
+                // (this.counter == this.images.length - 1) ? this.counter = 0 : this.counter++;
+
+                //? METODO CLASSICO
+                
+                if(this.counter == this.images.length - 1){
+                    this.counter = 0;
+                } else{
+                    this.counter++;
+                }
+                
+            }
+
         }
-    }
+    }    
 );
