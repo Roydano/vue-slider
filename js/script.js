91c401d0ac2f29de8701415729088a4d7777f8fe
Roydano <roy.dano90@gmail.com>
2021-06-22 17:03:37 +0200

    add click back -

--- js/script.js
+++ js/script.js
@@ -17,19 +17,39 @@ const app = new Vue(
 
         methods:{
 
+            //*manda avanti la foto al click e comincia da capo alla fine
+
             nextPhoto(){
+                
                 //! CON TERNARIA
 
                 // (this.counter == this.images.length - 1) ? this.counter = 0 : this.counter++;
 
                 //? METODO CLASSICO
-                
+
                 if(this.counter == this.images.length - 1){
                     this.counter = 0;
                 } else{
                     this.counter++;
                 }
                 
+            },
+
+            //*manda indietro la foto al click e comincia da capo alla fine
+
+            backPhoto(){
+
+                //! CON TERNARIA
+
+                // (this.counter == 0) ? this.counter = this.images.length - 1 : this.counter--;
+
+                //? METODO CLASSICO
+
+                if(this.counter == 0){
+                    this.counter = this.images.length - 1;
+                }else{
+                    this.counter--;
+                }
             }
 
         }
